5e7feb5dd839a7b780b36a1e5f99754b5fa05d94
dsomorov <dsomorov@gmail.com>
2023-11-06 14:09:02 -0600

    merge all games into single page

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -5,12 +5,12 @@ const Browse = () => {
   return (
     <div className="h-full w-full flex justify-center">
       <ul className="flex flex-col items-center">
-        {/* <Link href="/games/tictactoe" className="hover:underline">
+        <Link href="/games/tictactoe" className="hover:underline">
           TicTacToe
         </Link>
         <Link href="/games/hearts" className="hover:underline">
           Hearts
-        </Link> */}
+        </Link>
         <Link href="/games/abc" className="hover:underline">
           ABC_Test_Game
         </Link>
--- app/games/tictactoe/page.tsx
+++
@@ -1,8 +0,0 @@
-import GameContainer from "@/app/components/GameContainer";
-import React from "react";
-
-const TicTacToeGame = () => {
-  return <GameContainer game="tictactoe" />;
-};
-
-export default TicTacToeGame;
