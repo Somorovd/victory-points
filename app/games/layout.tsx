3a0198048d04f2e30e673ce94d9f775f68e5f919
dsomorov <dsomorov@gmail.com>
2023-11-16 17:49:36 -0600

    fix page freezing from too many requests

--- app/games/layout.tsx
+++ app/games/layout.tsx
@@ -1,17 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useGames, GameStore } from "@/hooks/use-games-store";
 import useStore from "@/hooks/use-store";
 
 const GamesMainLayout = ({ children }: { children: React.ReactNode }) => {
   const gameStore = useStore(useGames, (state) => state) as GameStore;
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (!gameStore) return;
+    if (!gameStore || isMounted) return;
     if (Object.keys(gameStore.allGames).length === 0) {
       gameStore.fetchGames();
     }
+    setIsMounted(true);
   }, [gameStore]);
 
   if (!gameStore) return null;
